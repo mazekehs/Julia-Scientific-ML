d1d3bafaaaae6b74e4f45b7db814557d4c0e0662
Abhishek <abhishekkumar23951@gmail.com>
2024-11-16 11:50:09 +0530

    Julia-First-Commit

---
+++ My first Julia code.jl
@@ -0,0 +1,64 @@
+println("Welcome to Julia. This is my first line of code")
+
+x=2
+typeof(x)
+
+y=2.5
+typeof(y)
+
+z='c'
+typeof(z)
+
+str="this is my room"
+typeof(str)
+
+str2="""this is also a string"""
+typeof(str2)
+
+
+😆=15
+🥶=2.8
+🥳='c'
+
+
+🥳-😆
+
+a=2
+b=3
+println(a*b)
+println(a/b)
+println(a^b)
+println(a>b)
+println(a!=b)
+
+name="Abhishek Mazewise"
+println("My name is $name")
+
+first_name="Abhishek"
+last_name="Mazewise"
+println("$first_name $last_name")
+println(first_name*" "*last_name)
+
+array1=[1,2,3,4,5]
+typeof(array1)
+
+array1[5]
+
+array1[3]=9
+array1
+
+tuple1=(1,2,3,4,5)
+typeof(tuple1) # tuple is immutable
+
+is_true=(2>3)||(5>2)
+
+tuple1=("1",2,'3',4,5.5)
+array1=[1,2,"3",4,5]
+
+println(tuple1)
+println(array1)
+
+println(typeof(tuple1))
+println(typeof(array1))
+
+
